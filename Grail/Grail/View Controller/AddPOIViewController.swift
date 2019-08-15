c41bb6c86ef7f37dbee47ec7b43c79013dc91b9a
Hayden Hastings <haydenhastings32@gmail.com>
2019-08-15 16:26:37 -0600

    Created Add file to add POIs

--- Grail/Grail/View Controller/AddPOIViewController.swift
+++ Grail/Grail/View Controller/AddPOIViewController.swift
@@ -8,23 +8,79 @@
 
 import UIKit
 
-class AddPOIViewController: UIViewController {
+protocol AddPOIDelegate {
+    func poiWasAdded(_ poi: POI)
+}
 
+class AddPOIViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    var delegate: AddPOIDelegate?
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var countryTextField: UITextField!
+    @IBOutlet weak var clue1TextFiled: UITextField!
+    @IBOutlet weak var clue2TextField: UITextField!
+    @IBOutlet weak var clue3TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        // Do any additional setup after loading the view.
+    
+    // MARK: - IBActions
+    
+    @IBAction func SaveButtonTapped(_ sender: Any) {
+        
+        guard let location = locationTextField.text,
+            let country = countryTextField.text,
+            !location.isEmpty,
+            !country.isEmpty else { return }
+        
+        var poi = POI(location: location, country: country, clues: [])
+        
+        if let clue1 = clue1TextFiled.text,
+            !clue1.isEmpty {
+            poi.clues.append(clue1)
+        }
+        
+        if let clue2 = clue2TextField.text,
+            !clue2.isEmpty {
+            poi.clues.append(clue2)
+        }
+        
+        if let clue3 = clue3TextField.text,
+            !clue3.isEmpty {
+            poi.clues.append(clue3)
+        }
+        
+        delegate?.poiWasAdded(poi)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
+}
 
+extension AddPOIViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if let text = textField.text,
+            !text.isEmpty {
+            switch textField {
+            case locationTextField:
+                countryTextField.resignFirstResponder()
+            case countryTextField:
+                clue1TextFiled.resignFirstResponder()
+            case clue1TextFiled:
+                clue2TextField.resignFirstResponder()
+            case clue2TextField:
+                clue3TextField.resignFirstResponder()
+            default:
+                textField.resignFirstResponder()
+            }
+        }
+        return false
+    }
 }
--- Grail/Grail/View Controller/POIsTableViewController.swift
+++ Grail/Grail/View Controller/POIsTableViewController.swift
@@ -10,21 +10,36 @@ import UIKit
 
 class POIsTableViewController: UIViewController {
 
+    // MARK: - Properties
+    var poi: [POI] = []
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+}
 
+// MARK: - UITableViewDataSource
+extension POIsTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
