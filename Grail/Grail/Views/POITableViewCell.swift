0f24c886421f264bfcd6ab07093a689a9809453f
Hayden Hastings <haydenhastings32@gmail.com>
2019-08-16 12:00:59 -0600

    Created the Cell file with code.

--- Grail/Grail/Views/POITableViewCell.swift
+++ Grail/Grail/Views/POITableViewCell.swift
@@ -9,16 +9,25 @@
 import UIKit
 
 class POITableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - Properties
+    var poi: POI? {
+        didSet {
+            
+        }
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // MARK: - IBOutlets
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var numberOfCluesLabel: UILabel!
+    
+    // MARK: - Methods
+    func updateViews() {
+        guard let poi = poi else { return }
+        
+        locationLabel.text = poi.location
+        countryLabel.text = poi.country
+        numberOfCluesLabel.text = "\(poi.clues.count)"
     }
-
 }
